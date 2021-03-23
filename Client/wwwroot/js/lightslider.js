b3bb296937806aa4d3a0d626d0ddf4cba524e0ad
ITTechs <30418672+lizbarnett10@users.noreply.github.com>
2021-03-23 11:03:47 -0500

    p

--- Client/wwwroot/js/lightslider.js
+++ Client/wwwroot/js/lightslider.js
@@ -1141,3 +1141,24 @@
         return this;
     };
 }(jQuery));
+
+
+export function Go() {
+    $("#content-slider").lightSlider({
+        loop: true,
+        keyPress: true
+    });
+    $('#image-gallery').lightSlider({
+        gallery: true,
+        item: 1,
+        thumbItem: 9,
+        pause: 3000,
+        slideMargin: 0,
+        speed: 1000,
+        auto: true,
+        loop: true,
+        onSliderLoad: function () {
+            $('#image-gallery').removeClass('cS-hidden');
+        }
+    });
+};
